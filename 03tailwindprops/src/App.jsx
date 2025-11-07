253f00eb675d25a3ea6db9ba96601163481f98b6
saad naikwade <naikwadesaad@gmail.com>
2025-11-07 21:39:13 +0530

    added 01 02 03 projects basics

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Card from "./components/Card";
+
+function App() {
+  const [count, setCount] = useState(0);
+  let myObj={
+    userName:"hitesh",
+    age:21 
+  }
+
+  return (
+    <>
+    <h1 className="bg-green-400 text-black p-4 rounded-2xl">Tailwind Test</h1>
+
+   <Card someObj={myObj} userName="saad" btnText="Click me →"/>
+   <Card userName="Naikwade" btnText="Click me →"/>
+   
+    </>
+  );
+}
+
+export default App;
